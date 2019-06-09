c1f6bc7ec9dcc5970fe1ccf94c951517affbc881
harishkumar <hv559q@att.com>
2019-06-09 13:59:10 -0400

    "updated port number to support cloud"

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
     res.send('This is our first application');
 });
 
-var server = app.listen('5000', function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     console.log("The application is running under port : " + 3000);
 });
